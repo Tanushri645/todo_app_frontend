b01dc07c23d009553532a6ac444aa418290d5244
Tanushri Das <ap8669207@gmail.com>
2024-12-04 16:22:28 +0530

    Frontend is done

---
+++ src/component/Footer/fotter.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./footer.css";
+
+export default function footer() {
+    return(
+        <div className="footer-text">
+            <p>Copyright disclaimer under Section 107 of the Copyright Act allows for 'fair use' of copyrighted material for purposes such as criticism, comment, news reporting, teaching, scholarship, or research, without the need for permission from the copyright owner.</p>
+        </div>
+);
+}
